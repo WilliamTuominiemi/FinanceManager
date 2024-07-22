3625fb6d8bfef0e21603b444408fb085bde349fc
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-07-22 23:49:31 +0300

    Save objects to csv

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,41 +1,82 @@
-﻿// MainWindow.xaml.cs
-using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using FinanceManager.Models;
+using System.Windows.Controls;
 
 namespace FinanceManager
 {
     public partial class MainWindow : Window
     {
-        private ObservableCollection<Transaction> transactions;
-        private int nextId = 1;
+        private List<Transaction> Transactions { get; set; }
+        private string filePath = "transactions.csv";
 
         public MainWindow()
         {
-            InitializeComponent();  
-            transactions = new ObservableCollection<Transaction>();
-            dataGridTransactions.ItemsSource = transactions;
+            InitializeComponent();
+            Transactions = new List<Transaction>();
+            LoadTransactions();
+            dataGridTransactions.ItemsSource = Transactions;
         }
 
         private void ButtonAddTransaction_Click(object sender, RoutedEventArgs e)
         {
-            var addTransactionWindow = new AddTransactionWindow(nextId);
+            var newTransactionId = Transactions.Count + 1;
+            var addTransactionWindow = new AddTransactionWindow(newTransactionId);
             if (addTransactionWindow.ShowDialog() == true)
             {
-                transactions.Add(addTransactionWindow.Transaction);
-                nextId++;
+                Transactions.Add(addTransactionWindow.Transaction);
+                SaveTransactions();
+                dataGridTransactions.Items.Refresh();
             }
         }
 
         private void ButtonDeleteTransaction_Click(object sender, RoutedEventArgs e)
         {
-            if (dataGridTransactions.SelectedItem is Transaction selectedTransaction)
+            var selectedTransaction = (Transaction)dataGridTransactions.SelectedItem;
+            if (selectedTransaction != null)
             {
-                transactions.Remove(selectedTransaction);
+                Transactions.Remove(selectedTransaction);
+                SaveTransactions();
+                dataGridTransactions.Items.Refresh();
             }
-            else
+        }
+
+        private void SaveTransactions()
+        {
+            using (var writer = new StreamWriter(filePath))
             {
-                MessageBox.Show("Please select a transaction to delete.", "Delete Transaction", MessageBoxButton.OK, MessageBoxImage.Warning);
+                writer.WriteLine("Id,Date,Description,Amount,Category");
+                foreach (var transaction in Transactions)
+                {
+                    writer.WriteLine($"{transaction.Id},{transaction.Date},{transaction.Description},{transaction.Amount},{transaction.Category}");
+                }
+            }
+        }
+
+        private void LoadTransactions()
+        {
+            if (!File.Exists(filePath))
+                return;
+
+            using (var reader = new StreamReader(filePath))
+            {
+                reader.ReadLine(); // Skip header
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var values = line.Split(',');
+                    var transaction = new Transaction
+                    {
+                        Id = int.Parse(values[0]),
+                        Date = DateTime.Parse(values[1]),
+                        Description = values[2],
+                        Amount = decimal.Parse(values[3]),
+                        Category = values[4]
+                    };
+                    Transactions.Add(transaction);
+                }
             }
         }
     }
